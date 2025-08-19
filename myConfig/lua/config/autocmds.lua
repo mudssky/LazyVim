fe12273b67132e93cc832e1c40e1fa632543cf33
mudssky <mudssky@gmail.com>
2025-08-19 17:38:18 +0800

    feat(markdown): 添加切换标题级别的快捷键支持
    为 markdown 文件添加 Ctrl+1 到 Ctrl+6 快捷键，支持在正常模式和插入模式下切换标题级别。当行已是相同级别标题时移除标记，否则设置为对应级别标题。优化光标位置以便立即输入文本。

--- myConfig/lua/config/autocmds.lua
+++ myConfig/lua/config/autocmds.lua
@@ -29,34 +29,61 @@ autocmd('Syntax', {
 
 
 -- 也可以用lsp的h1代替
--- autocmd('FileType', {
---   pattern = 'markdown',
---   group = augroup('markdown'),
---   callback = function()
---     for i = 1, 6 do
---       local heading = string.rep("#", i) .. " "
---       -- 为正常模式 (Normal Mode) 设置快捷键
---       vim.keymap.set("n", "<C-" .. i .. ">", function()
---         local line = vim.api.nvim_get_current_line()
---         -- 移除已有的标题标记，避免重复添加
---         local clean_line = string.gsub(line, "^#* ", "")
---         vim.api.nvim_set_current_line(heading .. clean_line)
---       end, { buffer = true, desc = "标题 " .. i .. " (Normal)" })
+-- 但是中文输入的情况下还是这些快捷键好用
 
---       -- 为插入模式 (Insert Mode) 设置快捷键
---       vim.keymap.set("i", "<C-" .. i .. ">", function()
---         -- 移动到行首
---         vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], 0 })
---         -- 移除已有的标题标记
---         local line = vim.api.nvim_get_current_line()
---         local clean_line = string.gsub(line, "^#* ", "")
---         vim.api.nvim_set_current_line(clean_line)
---         -- 插入新的标题标记
---         vim.api.nvim_put({ heading }, 'c', true, true)
---       end, { buffer = true, desc = "标题 " .. i .. " (Insert)" })
---     end
---   end
--- })
+-- 以下是markdown文件的快捷键设置
+-- 设置Ctrl+1到Ctrl+6快捷键用于切换/设置markdown标题级别
+-- 在正常模式和插入模式下都可以使用
+-- 如果当前行已经是对应级别的标题，则会移除标题标记
+-- 如果不是标题或者是其他级别的标题，则会设置为对应级别的标题
+-- 例如:
+-- 按下Ctrl+1: 普通文本 -> # 一级标题
+-- 再次按下Ctrl+1: # 一级标题 -> 普通文本
+-- 按下Ctrl+2: # 一级标题 -> ## 二级标题
+
+autocmd('FileType', {
+  pattern = 'markdown',
+  group = augroup('markdown'),
+  callback = function()
+    for i = 1, 6 do
+      -- 核心逻辑函数，用于计算新的行内容
+      local function get_new_line()
+        local line = vim.api.nvim_get_current_line()
+        -- 匹配行首的'#'号，并捕获它们
+        local current_hashes = line:match("^(#+)%s")
+        -- 计算当前标题级别，如果没有则为 0
+        local current_level = current_hashes and #current_hashes or 0
+        -- 移除所有已有的标题标记，得到纯净的文本行
+        local clean_line = line:gsub("^(#*)%s*", "")
+
+        -- 如果触发的快捷键级别与当前行标题级别相同
+        if current_level == i then
+          -- 取消标题，返回纯净文本
+          return clean_line
+        else
+          -- 否则，覆盖为新的标题级别
+          local new_heading = string.rep("#", i) .. " "
+          return new_heading .. clean_line
+        end
+      end
+
+      -- 为正常模式 (Normal Mode) 设置快捷键
+      vim.keymap.set("n", "<C-" .. i .. ">", function()
+        vim.api.nvim_set_current_line(get_new_line())
+      end, { buffer = true, desc = "切换/设置 标题 " .. i })
+
+      -- 为插入模式 (Insert Mode) 设置快捷键
+      vim.keymap.set("i", "<C-" .. i .. ">", function()
+        local new_line = get_new_line()
+        vim.api.nvim_set_current_line(new_line)
+        -- 将光标移动到标题标记之后，方便立即输入
+        local prefix = new_line:match("^(#*)%s*") or ""
+        local row = vim.api.nvim_win_get_cursor(0)[1]
+        vim.api.nvim_win_set_cursor(0, { row, #prefix })
+      end, { buffer = true, desc = "切换/设置 标题 " .. i })
+    end
+  end
+})
 
 -- 禁用 LazyVim 的拼写检查自动命令组
 -- vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
