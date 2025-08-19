61225fcd7417f9ccf434bab00e04dcc3b5c6cdde
mudssky <mudssky@gmail.com>
2025-08-19 17:23:11 +0800

    refactor(autocmds): 移除注释掉的markdown标题快捷键代码
    清理不再使用的代码段，保持配置文件简洁

--- myConfig/lua/config/autocmds.lua
+++ myConfig/lua/config/autocmds.lua
@@ -26,7 +26,38 @@ autocmd('Syntax', {
     custom_autocmd()
   end
 })
--- end
+
+
+-- 也可以用lsp的h1代替
+-- autocmd('FileType', {
+--   pattern = 'markdown',
+--   group = augroup('markdown'),
+--   callback = function()
+--     for i = 1, 6 do
+--       local heading = string.rep("#", i) .. " "
+--       -- 为正常模式 (Normal Mode) 设置快捷键
+--       vim.keymap.set("n", "<C-" .. i .. ">", function()
+--         local line = vim.api.nvim_get_current_line()
+--         -- 移除已有的标题标记，避免重复添加
+--         local clean_line = string.gsub(line, "^#* ", "")
+--         vim.api.nvim_set_current_line(heading .. clean_line)
+--       end, { buffer = true, desc = "标题 " .. i .. " (Normal)" })
+
+--       -- 为插入模式 (Insert Mode) 设置快捷键
+--       vim.keymap.set("i", "<C-" .. i .. ">", function()
+--         -- 移动到行首
+--         vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], 0 })
+--         -- 移除已有的标题标记
+--         local line = vim.api.nvim_get_current_line()
+--         local clean_line = string.gsub(line, "^#* ", "")
+--         vim.api.nvim_set_current_line(clean_line)
+--         -- 插入新的标题标记
+--         vim.api.nvim_put({ heading }, 'c', true, true)
+--       end, { buffer = true, desc = "标题 " .. i .. " (Insert)" })
+--     end
+--   end
+-- })
+
 -- 禁用 LazyVim 的拼写检查自动命令组
 -- vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 -- vim.notify("✓ 已禁用 LazyVim 拼写检查自动命令组 (wrap_spell)", vim.log.levels.INFO, { title = "MyConfig" })
